ac44d895f13a3fba6fd3d4ccad7a1fef1013101d
Reza Moradi <34216130+morapoly@users.noreply.github.com>
2021-05-24 15:14:22 +0430

    Update main.cpp

--- LegendaryValues/main.cpp
+++ LegendaryValues/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include "LegendaryValue.h"
+
+using namespace std;
+
+void printResults(std::vector<int> input, int lv) {
+    LegendaryValue legendaryValue;
+    vector<int> outputs = legendaryValue.run(move(input), lv);
+    cout << "outputs are: ";
+    for (int i : outputs) {
+        cout << i << " ";
+    }
+    cout << endl;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    printResults({1, 3, 4, 2, 9, 5, 9}, 10);
+    printResults({1, 2, 3, 4, 5, 6}, 7);
     return 0;
 }
