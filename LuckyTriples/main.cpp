068444f12a87715e85bd7491ea4a9ca444b0cdcc
Reza Moradi <34216130+morapoly@users.noreply.github.com>
2021-05-24 15:16:13 +0430

    Update main.cpp

--- LuckyTriples/main.cpp
+++ LuckyTriples/main.cpp
@@ -6,11 +6,12 @@ using namespace std;
 void printResults(std::vector<int> input) {
     LuckyTriple luckyTriple;
     auto output = luckyTriple.run(move(input));
-    cout << "output is : " << output;
+    cout << "output is : " << output << endl;
 }
 
 int main() {
     printResults({1, 1, 1});
     printResults({1, 2, 3, 4, 5, 6});
+    printResults({1, 2, 3, 4, 5, 6, 8});
     return 0;
 }
