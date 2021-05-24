41e4215d020cdfe4bf9e9486e10cc8e866b366db
Reza Moradi <34216130+morapoly@users.noreply.github.com>
2021-05-24 15:15:06 +0430

    Update LuckyTriple.cpp

--- LuckyTriples/LuckyTriple.cpp
+++ LuckyTriples/LuckyTriple.cpp
@@ -1,9 +1,24 @@
 #include "LuckyTriple.h"
 
 int LuckyTriple::run(std::vector<int> list) {
-    /**
-     * Complete this function
-     */
-
-    return 0;
+    int n{int(list.size())};
+    int counter{};
+    // we grab first
+    for (int i{}; i < n; i++){
+        // we grab second
+        for (int j{}; j < n; j++){
+            // checking out the second divides the first and deny the descending and equal solutions
+            if (j > i && list[j] % list[i] == 0){
+                // we grab the last
+                for (int k{}; k < n; k++){
+                    // checking out the last divides the second and deny the descending and equal solutions
+                    if (k > j && list[k] % list[j] == 0){
+                        // counting solutions
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+    return counter;
 }
